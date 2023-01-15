d1e9b30fa81f36fd3fb7bdc1cfd861f74cdd7110
peizong <45988544+peizong@users.noreply.github.com>
2023-01-15 16:28:00 -0500

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -4,6 +4,7 @@
 wget https://www.dropbox.com/s/tzl2zxzigbk2o9i/model_121520
 wget https://www.dropbox.com/s/tibgnb2higuzwca/model_121520.trainables.syn1neg.npy
 wget https://www.dropbox.com/s/zj1uw4mlka6y2zx/model_121520.wv.vectors.npy
+wget https://www.dropbox.com/s/ashco4x5wccc5kl/model_121520_phraser.pkl
 mv model_121520* alloy2vec/training/models/
 
 #download designed HEAs of 6 components
