57b27e1e24bf2bfa0167a462adb04f497dc4c1fd
peizong <45988544+peizong@users.noreply.github.com>
2023-02-28 09:39:56 -0500

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -3,6 +3,7 @@
 #download the model
 wget https://www.dropbox.com/s/tzl2zxzigbk2o9i/model_121520
 wget https://www.dropbox.com/s/tibgnb2higuzwca/model_121520.trainables.syn1neg.npy
+wget https://www.dropbox.com/s/8bz4h2dia7eop6h/model_121520.trainables.syn1.npy
 wget https://www.dropbox.com/s/zj1uw4mlka6y2zx/model_121520.wv.vectors.npy
 wget https://www.dropbox.com/s/ashco4x5wccc5kl/model_121520_phraser.pkl
 mv model_121520* alloy2vec/training/models/
